44cd32c74517cf47003cf82a4bee90e0ada2e611
dphakatkar <75238313+dphakatkar@users.noreply.github.com>
2021-01-01 12:03:50 +0530

    Add files via upload

---
+++ SQL/Activity1Verification.sql
@@ -0,0 +1,20 @@
+REM   Script: Activity1Verification
+REM   In this activity, we will be creating a table:
+First create a table in SQL using the CREATE command named salesman:
+Name            | Datatype
+----------------+-------------
+salesman_id     | int
+salesman_name   | varchar2(20)
+salesman_city   | varchar2(20)
+commission      | int
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+DESCRIBE salesman
+
+
---
+++ SQL/Activity2.sql
@@ -0,0 +1,32 @@
+REM   Script: Activity2
+REM   In this activity, we will be inserting values in the table created in the  Activity1.
+Once all the data has been inserted into the table, use the SELECT command to view the results.
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+DESCRIBE salesman
+
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
