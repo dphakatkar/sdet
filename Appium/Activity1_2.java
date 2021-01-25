9273ba4aea557d7c97114c92efc27433274a66e4
dphakatkar <75238313+dphakatkar@users.noreply.github.com>
2021-01-25 12:51:59 +0530

    Add files via upload

---
+++ Appium/Activity1_2.java
@@ -0,0 +1,32 @@
+package Appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class Activity1_2 {
+    @Test
+    public void calc() throws MalformedURLException {
+        // Set the Desired Capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "redme Note4");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appPackage", "com.oneplus.calculator");
+        caps.setCapability("appActivity", "com.oneplus.calculator.Calculator");
+
+        // Instantiate Appium Driver
+        AppiumDriver<MobileElement> driver = null;
+        // Initialize driver
+        driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+    	System.out.println("Calculator is open");
+        driver.quit();
+    
+    }
+
+}
